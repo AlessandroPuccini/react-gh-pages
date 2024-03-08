23ac8486400b36014a296179972451b6aa03bee8
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-03-08 14:12:48 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+// import AddUser from './AddUser';
+// import UsersList from './UsersList';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+const userList = [
+  {id: 1, nome: "Andrea Rossi", email: "andrea@email.it"},
+  {id: 2, nome: "Angelica Bianchi", email: "angelica@email.it"},
+  {id: 3, nome: "Mario Verdi", email: "mario@email.it"},
+];
+
+const [users, setUsers] = useState(userList);
+
+const handleAddUser = (newUser) => {
+ setUsers((prev) => [...prev, newUser]);
+};
+
+const handleDeleteUser = (idUser) => {
+  if(confirm ("sei sicuro di voler cancellare l'utente?")){
+    setUsers(users.filter(user => user.id !== idUser));
+  };
+
+const handleModifyUser = (idUser, newName) => {
+  const newUsers = users.map((user) => 
+  user.id === idUser ? {...user, nome: newName} : user );
+  setUsers(newUsers);
+};
+
+};
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Elenco Utenti</h1>
+      {/* <AddUser onAddUser={handleAddUser}/> 
+      <UsersList
+      users={users} 
+      onDeleteUser={handleDeleteUser} /> */}
+    </main>
   )
 }
 
